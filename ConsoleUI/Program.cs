09d1d4c297c8beddb192aed6f289a54c12a29340
Metee19 <77540484+Metee19@users.noreply.github.com>
2021-02-03 23:47:34 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{   //Open Closed Principle
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(10,50))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
